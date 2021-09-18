c8d4ad9f691b69d5ab34945fe1f56759b099a0e8
Michael Adam Groberman <michael.groberman@me.com>
2021-09-18 14:00:49 -0400

    updated appointments.js
    defined and wrote the appointments model

---
+++ models/appointments.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Appointments extends Model {}
+
+Appointments.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        time: {
+            type: DataTypes.TIME,
+            allowNull: false,
+        },
+        address: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        notes: {
+            type: DataTypes.STRING,
+            allowNull: true, 
+        },
+        pet_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'pet',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'appointments',
+    }
+);
