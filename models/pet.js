13b06c426f7fcf6a125fe4246e4931a4c7f567c7
Michael Adam Groberman <michael.groberman@me.com>
2021-09-18 13:47:57 -0400

    created pet.js
    created and defined pet model

---
+++ models/pet.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Pet extends Model {}
+
+Pet.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      type: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      breed: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      date_created: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'user',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'pet',
+    }
+  );
+  
+  module.exports = Pet;
