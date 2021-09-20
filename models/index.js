c0b0991240d25ee655498afc2c9769b926e67565
Ying Li <lliying1213@gmail.com>
2021-09-20 19:38:51 -0400

    update handlebars

--- models/index.js
+++ models/index.js
@@ -3,11 +3,11 @@ const Pet = require('./pet');
 const Notes = require('./notes');
 const Appointments = require('./appointments');
 const Vaccinations = require('./vaccinations');
-const Exercise = require('./exercise'); 
+const Exercise = require('./exercise');
 
 User.hasMany(Pet, {
   foreignKey: 'user_id',
-  onDelete: 'CASCADE'
+  onDelete: 'CASCADE',
 });
 
 Pet.belongsTo(User, {
@@ -15,39 +15,39 @@ Pet.belongsTo(User, {
 });
 
 Pet.hasMany(Notes, {
-    foreignKey: 'pet_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'pet_id',
+  onDelete: 'CASCADE',
 });
 
 Notes.belongsTo(Pet, {
-    foreignKey: 'pet_id',
+  foreignKey: 'pet_id',
 });
 
 Pet.hasMany(Appointments, {
-    foreignKey: 'pet_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'pet_id',
+  onDelete: 'CASCADE',
 });
 
 Appointments.belongsTo(Pet, {
-    foreignKey: 'pet_id,'
+  foreignKey: 'pet_id,',
 });
 
 Pet.hasMany(Vaccinations, {
-    foreignKey: 'pet_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'pet_id',
+  onDelete: 'CASCADE',
 });
 
 Vaccinations.belongsTo(Pet, {
-    foreignKey: 'pet_id,'
+  foreignKey: 'pet_id,',
 });
 
 Pet.hasMany(Exercise, {
-    foreignKey: 'pet_id',
-    onDelete: 'CASCADE'
+  foreignKey: 'pet_id',
+  onDelete: 'CASCADE',
 });
 
 Exercise.belongsTo(Pet, {
-    foreignKey: 'pet_id,'
+  foreignKey: 'pet_id,',
 });
 
 module.exports = { User, Pet, Notes, Appointments, Exercise, Vaccinations };
