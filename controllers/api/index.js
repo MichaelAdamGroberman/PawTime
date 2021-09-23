9399800f56d18b1ae48edca270a742ef03afd88d
Natalia Post <natalia.post11@gmail.com>
2021-09-22 23:14:45 -0400

    added vaccinationRoute

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const petRoutes = require('./petRoutes');
 const apptRoutes = require('./appointmentRoutes');
 const exerciseRoutes = require('./exerciseRoutes');
 const noteRoutes = require('./noteRoutes');
+const vaccinationRoutes = require('./vaccinationRoutes');
 
 router.use('/users', userRoutes);
 router.use('/pets', petRoutes);
 router.use('/appointments', apptRoutes);
 router.use('/exercises', exerciseRoutes);
 router.use('/notes', noteRoutes);
+router.use('/vaccinations', vaccinationRoutes);
 
 module.exports = router;
